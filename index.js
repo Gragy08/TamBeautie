0056b4045e12af1682c04abca92f1c203b0a558f
Gragy08 <giaqui1712@gmail.com>
2025-08-22 15:43:55 +0700

    add env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://giaqui1712:EDrFE61LVcnODHpT@cluster0.otsxhah.mongodb.net/tambeautie-spa');
+require('dotenv').config();
+mongoose.connect(process.env.DATABASE);
 
 const Blog = mongoose.model('Blog', {
     name: String
