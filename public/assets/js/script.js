220d61445f0fb7c27b55412d1ba2283fbc1daaa2
Gragy08 <giaqui1712@gmail.com>
2025-08-26 14:02:32 +0700

    homepage section1, scrolled nav

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,8 @@
-console.log("TamBeautie script loaded");
+window.addEventListener("scroll", function() {
+  const header = document.querySelector(".header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
