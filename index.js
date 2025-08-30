7e454c09c768243618bc46604d001d16382497ba
Gragy08 <giaqui1712@gmail.com>
2025-08-30 13:50:11 +0700

    deploy vercel

--- index.js
+++ index.js
@@ -5,21 +5,27 @@ const database = require('./config/database');
 const clientRoutes = require('./routes/client/index.route');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Connect to the database
 database.connect();
 
-//Set up Views
+// Set up Views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//Set up Static Files
+// Set up Static Files
 app.use(express.static(path.join(__dirname, "public")));
 
-//Set up links to client routes
+// Set up links to client routes
 app.use('/', clientRoutes);
 
-app.listen(port, () => {
-    console.log(`TamBeautie app listening at http://localhost:${port}`);
-})
+// 👉 Khi chạy local (node index.js) thì listen
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 TamBeautie app listening at http://localhost:${port}`);
+  });
+}
+
+// 👉 Khi deploy Vercel thì export app
+module.exports = app;
